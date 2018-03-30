41e003bcd7b4c63ffd572dd9ee56aea70aead638
hhy <1014596312@qq.com>
2018-03-30 23:50:26 +0800

    [B] 完成B练习

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,14 +1,16 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
+        List<String> result = new ArrayList<>(collection1);
         for (List<String> collction :
                 collection2) {
-            System.out.println(collection1);
+            result.retainAll(collction);
         }
-        return null;
+        return result;
     }
 }
