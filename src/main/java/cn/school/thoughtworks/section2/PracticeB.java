52a9bf4196c662e0da3bdfcc784882581fc84fdc
hhy <1014596312@qq.com>
2018-03-31 23:23:43 +0800

    [2-B] 重复2-A的代码，未处理d-5的情况

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,22 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> result = new HashMap<String, Integer>();
+        for (String item :
+                collection1) {
+            if (result.get(item)==(null)) {
+                result.put(item, 1);
+            }else {
+                Integer number = result.get(item);
+                result.put(item, ++number);
+            }
+        }
+        return result;
     }
 }
