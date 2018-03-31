5bf7eaad1e50ee80df2c6e4b7e144580362cbbe1
hhy <1014596312@qq.com>
2018-03-31 23:59:48 +0800

    [3-C] 结合3-A和3-B完成3-C

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,29 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> result = new HashMap<String, Integer>();
+        for (String item :
+                collectionA) {
+            if (result.get(item)==(null)) {
+                result.put(item, 1);
+            }else {
+                Integer number = result.get(item);
+                result.put(item, ++number);
+            }
+        }
+        for (String item : object.get("value")) {
+            Integer number = result.get(item);
+            if (number != null) {
+                number -= (number / 3);
+                result.put(item, number);
+            }
+        }
+        return result;
     }
 }
