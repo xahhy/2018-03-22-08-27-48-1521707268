3121eb69992c15092e92fdf984d38b63aff11e81
hhy <1014596312@qq.com>
2018-03-31 23:48:15 +0800

    [2-C] 处理不同的符号:[]-。完成C

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,32 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> result = new HashMap<String, Integer>();
+        for (String item :
+                collection1) {
+            String[] array = item.split("[-:\\[\\]]");
+            if (array.length > 1) {
+                increaseNumber(result, array[0], Integer.parseInt(array[1]));
+                continue;
+            }
+            increaseNumber(result, item, 1);
+        }
+        return result;
+    }
 
-        return null;
+    private void increaseNumber(Map<String, Integer> result, String item, Integer increment) {
+        if (result.get(item) == (null)) {
+            result.put(item, increment);
+        } else {
+            Integer number = result.get(item);
+            number += increment;
+            result.put(item, number);
+        }
     }
 }
